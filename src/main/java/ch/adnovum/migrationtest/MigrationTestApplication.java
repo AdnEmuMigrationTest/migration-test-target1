e5596841b84bfbc080bf61548836c210d022ad62
Sandro Heinzelmann <sandro.heinzelmann@adnovum.ch>
2024-03-08 13:25:27 +0100

    initial commit

---
+++ src/main/java/ch/adnovum/migrationtest/MigrationTestApplication.java
@@ -0,0 +1,13 @@
+package ch.adnovum.migrationtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigrationTestApplication.class, args);
+	}
+
+}
